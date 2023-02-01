42329288957289f853cc5ac081935c6f77e92397
Evgeniy <evgnec@gmail.com>
2023-02-01 19:27:28 +0200

    add

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -115,10 +115,10 @@ const genres = ['Jazz', 'Blues'];
 // будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
 
 // ```js
-const numbers = [2, 17, 94, 1, -5, 23, 37];
-let min = numbers[0];
-for (let i = 1; i < numbers.length; i++) {
-    min = Math.min(min, numbers[i]);
-}
-console.log(min); // 1
+// const numbers = [2, 17, 94, 1, -5, 23, 37];
+// let min = numbers[0];
+// for (let i = 1; i < numbers.length; i++) {
+//     min = Math.min(min, numbers[i]);
+// }
+// console.log(min); // 1
 // ```
