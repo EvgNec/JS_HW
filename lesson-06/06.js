cff782890a46571697715ba89937b94e50c8b222
Evgeniy <evgnec@gmail.com>
2023-02-08 21:20:48 +0200

    Update 06.js

--- lesson-06/06.js
+++ lesson-06/06.js
@@ -99,6 +99,7 @@
 // ```js
 // // Решение
 // function getStockReport({ companyName, stock }) {
+//    // { companyName, stock }
 //   let total = 0;
 //   for (const value of Object.values(stock)) {
 //     total += value;
@@ -171,15 +172,15 @@
 
 // ```js
 // // Решение
-// function transformUsername({ firstName, lastName, ...otherProps }) {
-//   return {
-//     fullName: `${firstName} ${lastName}`,
-//     ...otherProps,
+// function transformUsername({ firstName, lastName, ...other }) {
+//     return {
+//     ...other,
+//     fullName: `${firstName} ${lastName}`, 
 //   };
 // }
 
 // console.log(
-//   transformId({
+//   transformUsername({
 //     id: 1,
 //     firstName: 'Jacob',
 //     lastName: 'Mercer',
@@ -189,7 +190,7 @@
 // );
 
 // console.log(
-//   transformId({
+//   transformUsername({
 //     id: 2,
 //     firstName: 'Adrian',
 //     lastName: 'Cross',
