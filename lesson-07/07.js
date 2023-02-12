8ddddeab3e7c3ff1d538f5d23f5cf9c724979410
Evgeniy <evgnec@gmail.com>
2023-02-12 11:47:05 +0200

    30/41

--- lesson-07/07.js
+++ lesson-07/07.js
@@ -1,38 +1,18 @@
+function makeTask(data) {
+  // const completed = false;
+  // const category = "General";
+  // const priority = "Normal";
+  // Change code below this line
+  // let text = '';
+  const {
+    category = "General",
+    priority = "Normal",
+    completed = false,
+    text,
+  } = data;
+  return { category, priority, text , completed};
+  // Change code above this line
+}
 
-
-const forecast = {
-  today: {
-    low: 28,
-    high: 32,
-    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
-  },
-  tomorrow: {
-    low: 27,
-    high: 31,
-  },
-};
-// Change code below this line
-
-// const highToday = forecast.today.high;
-// const lowToday = forecast.today.low;
-// const todayIcon = forecast.today.icon;
-
-// const highTomorrow = forecast.tomorrow.high;
-// const lowTomorrow = forecast.tomorrow.low;
-// const tomorrowIcon = forecast.tomorrow.icon;
-
-const {
-  today: {
-    high: highToday,
-    low: lowToday,
-    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
-  },
-  tomorrow: {
-    low: lowTomorrow,
-    high: highTomorrow,
-    tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
-  }
-} = forecast;
-
-
-
+console.log(makeTask({}));
+console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }));
