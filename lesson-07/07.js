4981aaafdd3af8d5adedd19c1db01a28db436839
Evgeniy <evgnec@gmail.com>
2023-02-20 00:09:05 +0200

    14/48

--- lesson-07/07.js
+++ lesson-07/07.js
@@ -1,11 +1,14 @@
-const calculateTotalPrice = orderedItems => {
-  let totalPrice = 0;
-
-  orderedItems.forEach((item) =>{
-    totalPrice += item;
-  });
-
-  return totalPrice;
+function changeEven(numbers, value) {
+  // Change code below this line
+    const newNumbers = numbers.slice(0);
+    console.log(newNumbers);
+    for (let i = 0; i < newNumbers.length; i += 1) {
+    if (newNumbers[i] % 2 === 0) {
+      newNumbers[i] = newNumbers[i] + value;
+    }
+  }
+  return newNumbers;
+  // Change code above this line
 }
 
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(changeEven([17, 24, 68, 31, 42], 100));
