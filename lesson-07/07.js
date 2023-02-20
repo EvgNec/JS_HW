94ea9180aee7cd0d398008e0c8da3a0249004184
Evgeniy <evgnec@gmail.com>
2023-02-20 21:07:21 +0200

    15/48

--- lesson-07/07.js
+++ lesson-07/07.js
@@ -1,7 +1,6 @@
 function changeEven(numbers, value) {
   // Change code below this line
     const newNumbers = numbers.slice(0);
-    console.log(newNumbers);
     for (let i = 0; i < newNumbers.length; i += 1) {
     if (newNumbers[i] % 2 === 0) {
       newNumbers[i] = newNumbers[i] + value;
