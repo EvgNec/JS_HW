491556831c5ac599fec01a57c89957470cea7b41
Evgeniy <evgnec@gmail.com>
2023-02-21 22:32:45 +0200

    48/48

--- lesson-07/07.js
+++ lesson-07/07.js
@@ -1,33 +1,93 @@
-const books = [
+const users = [
   {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
   },
   {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
   {
-    title: "The Dreams in the Witch House",
-    author: "Howard Lovecraft",
-    rating: 8.67,
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
   },
-];
-const MIN_BOOK_RATING = 8;
-// Change code below this line
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
 
-const names = [...books]
-  .filter(book => book.rating>MIN_BOOK_RATING)
-  .map(book => book.author)
-  .sort((a, b) => a.author.localeCompare(b.author))
-  ;
+// const names = [...books]
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .sort((a, b) => a.localeCompare(b));
+// const getNamesSortedByFriendCount =
+//   [...users]
+//     .sort((a, b) => a.friends.length - b.friends.length)
+//   .map(user => user.name)
+// ;
+// console.log(getNamesSortedByFriendCount);
 
-console.log(names);
+
+// const getSortedFriends =
+//   [...users].flatMap(user => user.friends)
+//     .filter((fr, index, array) => array.indexOf(fr) === index)
+//     .sort((a,b) => a.localeCompare(b))
+// ;
+// // allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
+// console.log(getSortedFriends);
+const  gender="male";
+
+const getTotalBalanceByGender = 
+  [...users]
+  .filter(user => user.gender === gender)
+  .reduce((acc, user) => acc + user.balance,0) ;
+
+console.log(getTotalBalanceByGender);
