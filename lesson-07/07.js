3185da06a3d5ed8743e54cda1fa8d2e3e860e9a9
Evgeniy <evgnec@gmail.com>
2023-02-22 20:43:40 +0200

    add

--- lesson-07/07.js
+++ lesson-07/07.js
@@ -1,93 +1,362 @@
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male"
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female"
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male"
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female"
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
-    isActive: true,
-    balance: 3951,
-    gender: "male"
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
-    isActive: false,
-    balance: 1498,
-    gender: "male"
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female"
-  }
-]
+// # Модуль 4. Занятие 7. Коллбеки. Стрелочные функции. forEach
 
-// const names = [...books]
-//   .filter(book => book.rating > MIN_BOOK_RATING)
-//   .map(book => book.author)
-//   .sort((a, b) => a.localeCompare(b));
-// const getNamesSortedByFriendCount =
-//   [...users]
-//     .sort((a, b) => a.friends.length - b.friends.length)
-//   .map(user => user.name)
-// ;
-// console.log(getNamesSortedByFriendCount);
+// ## Example 1 - Коллбек функции
+
+// Напишите следующие функции:
+
+// - `createProduct(obj, callback)` - принимает объект товара без id, а также
+//   колбек. Функция создаёт обьект товара, добавляя ему уникальный идентификатор в
+//   свойство `id` и вызывает колбек передавая ему созданный обьект.
+// - `logProduct(product)` - коллбек принимающий обьект продукта и логирующий его в
+//   консоль
+// - `logTotalPrice(product)` - коллбек принимающий обьект продукта и логирующий
+//   общую стоимость товара в консоль
+
+// const products = [
+//   {
+//     name: "apples",
+//     price: 15,
+//     qty: 30,
+//   },
+//     {
+//     name: "orange",
+//     price: 40,
+//     qty: 50,
+//   },
+// ]
+
+// function logProduct(product) {
+//   console.log(`Name: ${product.name}. Price: ${product.price}. Quantity: ${product.qty} kg`);
+// }
+// // logProduct(products[1]);
+
+// function logProduct2({name, price, qty}) {
+//   console.log(`Name: ${name}. Price: ${price}. Quantity: ${qty} kg`);
+// }
+// // logProduct2(products[1]);
 
 
-// const getSortedFriends =
-//   [...users].flatMap(user => user.friends)
-//     .filter((fr, index, array) => array.indexOf(fr) === index)
-//     .sort((a,b) => a.localeCompare(b))
-// ;
-// // allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
-// console.log(getSortedFriends);
-const  gender="male";
+// function logTotalPrice({name,  price, qty }) {
+//    console.log(`Name: ${name}. Total Price: ${price*qty}`);
+// }
+// // logTotalPrice(products[1]);
 
-const getTotalBalanceByGender = 
-  [...users]
-  .filter(user => user.gender === gender)
-  .reduce((acc, user) => acc + user.balance,0) ;
+// function createProduct(obj, callback) {
+//   const product = { ...obj, id: Date.now() };
+//   callback(product);
+// }
 
-console.log(getTotalBalanceByGender);
+// createProduct(products[1], logTotalPrice);
+// createProduct(products[1], logProduct);
+// ```js
+// // Решение
+// function createProduct(partialProduct, callback) {
+//   const product = { id: Date.now(), ...partialProduct };
+//   callback(product);
+// }
+
+// function logProduct(product) {
+//   console.log(product);
+// }
+
+// function logTotalPrice(product) {
+//   console.log(product.price * product.quantity);
+// }
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+// ```
+
+// ## Example 2 - Коллбек функции
+
+// Добавьте объекту `account` методы `withdraw(amount, onSuccess, onError)` и
+// `deposit(amount, onSuccess, onError)`, где первый параметр это сумма операции, а
+// второй и третий - колбеки.
+
+// Метод `withdraw` вызывает onError если amount больше TRANSACTION_LIMIT или
+// this.balance, и onSuccess в противном случае.
+
+// Метод `deposit` вызывает onError если amount больше TRANSACTION_LIMIT или меньше
+// либо равен нулю, и onSuccess в противном случае.
+
+const TRANSACTION_LIMIT = 1000;
+
+const account = {
+  username: "Alex",
+  balance: 1500,
+  withdraw(amount, onSuccess, onError) {
+    if (amount > TRANSACTION_LIMIT) {
+      console.log(`Перевищен поточний ліміт ${TRANSACTION_LIMIT}`);
+    }
+     else if (this.balance < amount) {
+      console.log(`Недостатьно коштів, поточний баланс ${this.balance}`);
+    }
+    else {
+      this.balance -= amount;
+      console.log(`Успішно знято ${amount}, поточний баланс ${this.balance}`);
+    }
+  },
+  deposit(amount, onSuccess, onError) {
+    
+  },
+};
+
+console.log(account);
+account.withdraw(500);
+account.withdraw(100);
+account.withdraw(200);
+account.withdraw(500);
+account.withdraw(500);
+
+
+// ```js
+// // Решение
+// const TRANSACTION_LIMIT = 1000;
+
+// const account = {
+//   username: 'Jacob',
+//   balance: 400,
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount > this.balance) {
+//       onError(`Amount can't exceed account balance of ${this.balance} credits`);
+//     } else {
+//       this.balance -= amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+//   deposit(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount <= 0) {
+//       onError(`Amount must be more than 0 credits`);
+//     } else {
+//       this.balance += amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+// };
+
+// function handleSuccess(message) {
+//   console.log(`✅ Success! ${message}`);
+// }
+// function handleError(message) {
+//   console.log(`❌ Error! ${message}`);
+// }
+
+// account.withdraw(2000, handleSuccess, handleError);
+// account.withdraw(600, handleSuccess, handleError);
+// account.withdraw(300, handleSuccess, handleError);
+// account.deposit(1700, handleSuccess, handleError);
+// account.deposit(0, handleSuccess, handleError);
+// account.deposit(-600, handleSuccess, handleError);
+// account.deposit(600, handleSuccess, handleError);
+// ```
+
+// ## Example 3 - Коллбек функции
+
+// Напишите функцию `each(array, callback)`, которая первым параметром ожидает
+// массив, а вторым - функцию, которая применится к каждому элементу массива.
+// Функция each должна вернуть новый массив, элементами которого будут результаты
+// вызова коллбека.
+
+// ```js
+// // Решение
+// function each(array, callback) {
+//   const newArr = [];
+//   for (const el of array) {
+//     newArr.push(callback(el));
+//   }
+//   return newArr;
+// }
+
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return value * 2;
+//   }),
+// );
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return value - 10;
+//   }),
+// );
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return Math.sqrt(value);
+//   }),
+// );
+// console.log(
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//     return Math.ceil(value);
+//   }),
+// );
+// console.log(
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//     return Math.floor(value);
+//   }),
+// );
+// ```
+
+// ## Example 4 - Стрелочные функции
+
+// Выполните рефакторинг кода используя стрелочные функции.
+
+// ```js
+// function createProduct(partialProduct, callback) {
+//   const product = { id: Date.now(), ...partialProduct };
+//   callback(product);
+// }
+
+// function logProduct(product) {
+//   console.log(product);
+// }
+
+// function logTotalPrice(product) {
+//   console.log(product.price * product.quantity);
+// }
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+// ```
+
+// ## Example 5 - Стрелочные функции
+
+// Выполните рефакторинг кода используя стрелочные функции.
+
+// ```js
+// const TRANSACTION_LIMIT = 1000;
+
+// const account = {
+//   username: 'Jacob',
+//   balance: 400,
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount > this.balance) {
+//       onError(`Amount can't exceed account balance of ${this.balance} credits`);
+//     } else {
+//       this.balance -= amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+//   deposit(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount <= 0) {
+//       onError(`Amount must be more than 0 credits`);
+//     } else {
+//       this.balance += amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+// };
+
+// function handleSuccess(message) {
+//   console.log(`✅ Success! ${message}`);
+// }
+// function handleError(message) {
+//   console.log(`❌ Error! ${message}`);
+// }
+
+// account.withdraw(2000, handleSuccess, handleError);
+// account.withdraw(600, handleSuccess, handleError);
+// account.withdraw(300, handleSuccess, handleError);
+// account.deposit(1700, handleSuccess, handleError);
+// account.deposit(0, handleSuccess, handleError);
+// account.deposit(-600, handleSuccess, handleError);
+// account.deposit(600, handleSuccess, handleError);
+// ```
+
+// ## Example 6 - Инлайн стрелочные функции
+
+// Выполните рефакторинг кода используя стрелочные функции.
+
+// ```js
+// function each(array, callback) {
+//   const newArr = [];
+//   for (const el of array) {
+//     newArr.push(callback(el));
+//   }
+//   return newArr;
+// }
+
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return value * 2;
+//   }),
+// );
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return value - 10;
+//   }),
+// );
+// console.log(
+//   each([64, 49, 36, 25, 16], function (value) {
+//     return Math.sqrt(value);
+//   }),
+// );
+// console.log(
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//     return Math.ceil(value);
+//   }),
+// );
+// console.log(
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//     return Math.floor(value);
+//   }),
+// );
+// ```
+
+// ## Example 7 - Метод forEach
+
+// Выполните рефакторинг кода используя метод `forEach` и стрелочные функции.
+
+// ```js
+// function logItems(items) {
+//   console.log(items);
+//   for (let i = 0; i < items.length; i += 1) {
+//     console.log(`${i + 1} - ${items[i]}`);
+//   }
+// }
+
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+// ```
+
+// ## Example 8 - Метод forEach
+
+// Выполните рефакторинг кода используя метод `forEach` и стрелочные функции.
+
+// ```js
+// function printContactsInfo({ names, phones }) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+// ```
+
+// ## Example 9 - Метод forEach
+
+// Выполните рефакторинг кода используя метод `forEach` и стрелочные функции.
+
+// ```js
+// function calсulateAverage(...args) {
+//   let total = 0;
+//   for (let i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+//   return total / args.length;
+// }
+
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calсulateAverage(14, 8, 2)); // 8
+// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+// ```
