05fd7f1acef1f2f8315a9ca29059523685d21540
Evgeniy <evgnec@gmail.com>
2023-02-28 09:12:33 +0200

    get set

--- hw_all.js
+++ hw_all.js
@@ -1971,14 +1971,68 @@
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"
 // ==================================================================================
 // ==================================================================================
-//
-// ----------------------------------------------------------------------------------
+//Напиши класс StringBuilder, который принимает один параметр initialValue - произвольную строку, которая записывается на создаваемый объект в свойство value.
 
+// Объяви следующие методы класса:
+
+// getValue() - возвращает текущее значение свойства value.
+// padEnd(str) - получает параметр str (строку) и добавляет её в конец значения свойства value объекта который вызывает этот метод.
+// padStart(str) - получает параметр str (строку) и добавляет её в начало значения свойства value объекта который вызывает этот метод.
+// padBoth(str) - получает параметр str (строку) и добавляет её в начало и в конец значения свойства value объекта который вызывает этот метод.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+// ----------------------------------------------------------------------------------
+// class StringBuilder {
+//   constructor(initialValue){
+//     this.value = initialValue;
+//   }
+//   getValue(){return this.value;}
+//   padEnd(str){ this.value = this.value + str;}
+//   padStart(str){ this.value = str + this.value;}
+//   padBoth(str){ this.value = str + this.value + str;}  
+// }
+
+
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
 // ==================================================================================
 // ==================================================================================
 //
 // ----------------------------------------------------------------------------------
+// class Storage {
+//   // Change code below this line
+// #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
 
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
 // ==================================================================================
 // ==================================================================================
 //
