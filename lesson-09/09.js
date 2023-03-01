aa443043c111cd9b634d175174f74d62bc638e6b
Evgeniy <evgnec@gmail.com>
2023-03-01 14:28:36 +0200

    hw9

--- lesson-09/09.js
+++ lesson-09/09.js
@@ -1,48 +1,87 @@
-class User {
-  email;
+// # Модуль 5. Занятие 9. Контекст вызова функции и this
 
-  constructor(email) {
-    this.email = email;
-  }
+// ## Example 1 - Мастерская драгоценностей
 
-  get email() {
-    return this.email;
-  }
+// Напишите метод `calcTotalPrice(stoneName)`, который принимает название камня и
+// рассчитывает и возвращает общую стоимость камней с таким именем, ценой и
+// количеством из свойства `stones`.
 
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
-class Admin extends User {
-  // Change code below this line
-blacklistedEmails = [];
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
+// ```js
+const chopShop = {
+  stones: [
+    { name: 'Emerald', price: 1300, quantity: 4 },
+    { name: 'Diamond', price: 2700, quantity: 3 },
+    { name: 'Sapphire', price: 1400, quantity: 7 },
+    { name: 'Ruby', price: 800, quantity: 2 },
+        { name: 'Emerald', price: 1700, quantity: 5 },
+  ],
+  calcTotalPrice(stoneName) {
+    // console.log(" this.stones", this.stones);
+      let sum = 0;
+    this.stones.forEach(stone => {
 
-  constructor({ email, accessLevel }) {
-    super(email);
-    this.accessLevel = accessLevel;
-  }
-blacklist(email) {
- this.blacklistedEmails.push(email);
-}
-isBlacklisted(email) {
-return this.blacklistedEmails.includes(email);
-}
-  // Change code above this line
-}
+      if (stoneName === stone.name) {
+        sum += stone.price * stone.quantity;
+      }
+      // console.log("sum", sum);
+    });
+return sum;
+  },
+};
 
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
+console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+console.log(chopShop.calcTotalPrice('Ruby')); // 1600
+// ```
 
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
+// ## Example 2 - Телефонная книга
 
- mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
+// Выполните рефакторинг методов объекта `phonebook` чтобы код заработал.
+
+// ```js
+// const phonebook = {
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: generateId(),
+//       createdAt: getDate(),
+//     };
+//     contacts.push(newContact);
+//   },
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//   },
+//   getDate() {
+//     return Date.now();
+//   },
+// };
+
+// console.log(
+//   phonebook.add({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   phonebook.add({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+// );
+// ```
+
+// ## Example 3 - Калькулятор
+
+// Создайте объект `calculator` с тремя методами:
+
+// - `read(a, b)`- принимает два значения и сохраняет их как свойства объекта.
+// - `add()` - возвращает сумму сохранённых значений.
+// - `mult()` - перемножает сохранённые значения и возвращает результат.
+
+// ```js
+// const calculator = {};
+// ```
