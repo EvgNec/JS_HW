490644c90cdc49a5bcbc1a74c412f7c055aade0d
Evgeniy <evgnec@gmail.com>
2024-05-18 20:41:40 +0300

    upd

--- klass_ext.js
+++ klass_ext.js
@@ -1,5 +1,5 @@
 class Hero {
-    constructor(name = 'hero', xp = 0) {
+    constructor({name = 'hero', xp = 0} = {}) {
         this.name = name;
         this.xp = xp;
     }
@@ -11,16 +11,24 @@ class Hero {
 }
 
 class Warrior extends Hero {
-    constructor(name, xp, weapon) {
-        super(name, xp);
-        console.log("config", name, xp);
+        // constructor({ name, xp, weapon }) {
+        // super({ name, xp });
+    constructor({ weapon, ...restProps }={}) {
+        super(restProps);
         this.weapon = weapon;
-}   
+    }  
+    attack(damage) {
+        console.log(`${this.name} attack ${this.weapon} on ${damage}`)
+        this.xp -= damage;
+}    
 }
 
-const mango = new Warrior('mango', 1000, 'alebarda');
+const mango = new Warrior({name: 'mango', xp: 1000, weapon: 'alebarda'});
 console.log("mango", mango)
 
 mango.gainXp(1000);
+mango.gainXp(1000);
+mango.gainXp(1000);
+mango.attack(550);
+console.log("mango", mango)
 
-console.log(mango.__proto__)
