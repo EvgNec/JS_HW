59b5ead456e063faff2c990d48ec734a2d11ecfe
Evgeniy <evgnec@gmail.com>
2024-05-21 23:36:26 +0300

    upd

--- test.js
+++ test.js
@@ -15,15 +15,25 @@
 // const listItems = list.children;
 // console.log(listItems);
 
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
-const list = document.querySelector(".list");
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const list = document.querySelector(".list");
 
-const markup = technologies
-  .map((technology) => `<li class="list-item">${technology}</li>`)
-  .join("");
+// const markup = technologies
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//   .join("");
 
-// Check the console, you'll see a single string with HTML tags
-console.log(markup);
+// // Check the console, you'll see a single string with HTML tags
+// console.log(markup);
 
-// Adding all the markup in one operation
-list.innerHTML = markup;
+// // Adding all the markup in one operation
+// list.innerHTML = markup;
+
+// const button = document.querySelector(".btn");
+
+// const handleClick = (event) => {
+//   console.log("event: ", event);
+//   console.log("event type: ", event.type);
+//   console.log("currentTarget: ", event.currentTarget);
+// };
+
+// button.addEventListener("click", handleClick);
