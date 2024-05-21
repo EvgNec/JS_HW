ee34ee76cde76bad27198d72ac734277d0365194
Evgeniy <evgnec@gmail.com>
2024-05-21 23:38:02 +0300

    upd

--- test.js
+++ test.js
@@ -37,3 +37,14 @@
 // };
 
 // button.addEventListener("click", handleClick);
+
+
+const form = document.querySelector(".register-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { username, password }
+  } = event.currentTarget;
+  console.log(username.value, password.value);
+});
