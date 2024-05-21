7a46c8c7fab2c48dbf4da81161dcc3ba1e1a8a10
Evgeniy <evgnec@gmail.com>
2024-05-21 23:19:51 +0300

    upd

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
-console.log(document);
+// console.log(document);
 
-const body = document.body;
-console.log(body);
+// const body = document.body;
+// console.log(body);
 
+// const list = document.querySelector(".list");
+// console.log(list);
+
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+// const lastListItem = list.lastElementChild;
+// console.log(lastListItem);
+
+// const listItems = list.children;
+// console.log(listItems);
+
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
 const list = document.querySelector(".list");
-console.log(list);
 
-const firstListItem = list.firstElementChild;
-console.log(firstListItem);
+const markup = technologies
+  .map((technology) => `<li class="list-item">${technology}</li>`)
+  .join("");
 
-const lastListItem = list.lastElementChild;
-console.log(lastListItem);
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
 
-const listItems = list.children;
-console.log(listItems);
+// Adding all the markup in one operation
+list.innerHTML = markup;
